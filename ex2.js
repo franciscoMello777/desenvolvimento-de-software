6b736c2fc4c28484187cf67513be67bed979b78a
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-03-25 16:12:55 -0300

    Add files via upload

---
+++ ex2.js
@@ -0,0 +1,12 @@
+var idade = 18;
+var status = idade >= 18?"adulto":"menor de idade";
+print(status);
+
+var idade = 16;
+var status = idade >= 18?"adulto":"menor de idade";
+print(status);
+
+var pl1='alfa';
+var pl2='beto';
+resuldado=pl1+pl2;
+print(resuldado)
