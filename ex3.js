11b6ee2155756e51e14b94cbfb1f1a022619325e
franciscoMello777 <162368791+franciscoMello777@users.noreply.github.com>
2024-03-25 17:08:26 -0300

    Add files via upload

---
+++ ex3.js
@@ -0,0 +1,53 @@
+//1
+
+var a = 15;
+var b = 5;
+
+if (( a > 10 && a < 20) || b == 5)
+print ('true');
+else
+print('false')
+
+//2
+
+var a2 = 15;
+var b2 = 5;
+
+if ((a2 > 10 ) || b2 == 5)
+print('true');
+else
+print('false')
+
+//3
+
+var a3 = 10;
+var b3 = 5;
+var c3 = 15;
+
+if (((a3 > 10 && a3 < 20) || b3 == 5) || c3 == 10)
+print('true');
+else
+('false')
+
+//4
+
+var a4 = 10;
+var b4 = 5;
+var c4 = 15;
+
+if (a4 > 10 || b4 == 5 || c4 != 10)
+print('true');
+else
+('false')
+
+//5
+
+var a5 = 10;
+var b5 = 5;
+var c5 = 15;
+var d5 = 20;
+
+if ((a5 > 10 || b5 == 5) || c5 != 10 || d5 != 20 )
+print('true');
+else
+('false')
